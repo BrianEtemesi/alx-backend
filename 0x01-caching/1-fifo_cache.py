ea677253aeb8e93ae7568c778a9169a147307ac1
BrianEtemesi <etemecci@gmail.com>
2023-07-27 12:41:02 +0000

    fix pycode error

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -22,7 +22,7 @@ class FIFOCache(BaseCaching):
         max_items = BaseCaching.MAX_ITEMS
         if key and item:
             self.cache_data[key] = item
-            if len(self.cache_data) >  max_items:
+            if len(self.cache_data) > max_items:
                 first_key = next(iter(self.cache_data))
                 del self.cache_data[first_key]
                 print("DISCARD: {}".format(first_key))
