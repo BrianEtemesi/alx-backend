39743927320b87bb68bd42014c7a8af1e8691683
BrianEtemesi <etemecci@gmail.com>
2023-09-06 10:20:56 +0000

    comment code

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -17,20 +17,25 @@ client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.message);
 });
 
+// function to set a new school name and its value to redis
 const setNewSchool = (schoolName, value) => {
   client.SET(schoolName, value, redis.print);
 };
 
+// function to display the value of a school name stored in redis
 const displaySchoolValue = (schoolName) => {
   client.GET(schoolName, (err, value) => {
     console.log(value);
   });
 };
 
+// Promisify the 'displaySchoolValue' function
 const DisplaySchoolValueAsync = util.promisify(displaySchoolValue);
 
+// Define an asynchronous function to display the school value
 async function displaySchValue(schoolName) {
   try {
+    // Use the promisified 'displaySchoolValue' function to get the value
     const value = await DisplaySchoolValueAsync(schoolName);
     console.log(value);
   } catch (error) {
