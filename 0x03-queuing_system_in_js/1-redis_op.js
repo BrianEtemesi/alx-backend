98dd27c88272ff59fd6638e2350079340ee73e33
BrianEtemesi <etemecci@gmail.com>
2023-09-06 10:12:52 +0000

    comment code

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -15,16 +15,23 @@ client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.message);
 });
 
+// define a function to set a new school name and it value in redis
 const setNewSchool = (schoolName, value) => {
   client.SET(schoolName, value, redis.print);
 };
 
+// function to display the value of a schoolname stored in redis
 const displaySchoolValue = (schoolName) => {
   client.GET(schoolName, (err, value) => {
     console.log(value);
   });
 };
 
+// display value of 'Holberton' key in redis
 displaySchoolValue('Holberton');
+
+// set a new key 'HolbertonSanFrancisco' with a value '100' in redis
 setNewSchool('HolbertonSanFrancisco', '100');
+
+// display the value of 'HolbertonSanFrancisco' key in redis
 displaySchoolValue('HolbertonSanFrancisco');
